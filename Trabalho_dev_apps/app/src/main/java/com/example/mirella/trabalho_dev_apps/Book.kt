14693d5cfe698a48dafd85d7ff8ef1b142726794
DESKTOP-A5KE19V\Mirella <mirella.lds@gmail.com>
2018-06-06 18:58:06 -0300

    Remove all files

--- Trabalho_dev_apps/app/src/main/java/com/example/mirella/trabalho_dev_apps/Book.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mirella.trabalho_dev_apps
-
-import android.graphics.drawable.Drawable
-
-data class Book(var title: String, var description: String, var image: Int)
--- Trabalho_dev_apps/app/src/main/java/com/example/mirella/trabalho_dev_apps/Report.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mirella.trabalho_dev_apps
-
-import android.graphics.drawable.Drawable
-
-data class Report(var image: Drawable)
